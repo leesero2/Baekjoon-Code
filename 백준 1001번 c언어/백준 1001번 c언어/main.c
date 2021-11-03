aba89a83393b387425974513df9eea9fac6c80f2
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-03 19:26:57 +0900

    Add files via upload

---
+++ 백준 1001번 c언어/백준 1001번 c언어/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  백준 1001번 c언어
+//
+//  Created by 이지훈 on 2021/11/03.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",a-b);
+    return 0;
+}
