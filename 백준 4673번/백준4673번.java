b34238d2b95cf6223d93c2ded24683a7932fb631
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-28 22:51:08 +0900

    백준 4673번 자바
    백준 함수 문제

---
+++ 백준 4673번/백준4673번.java
@@ -0,0 +1,35 @@
+package 백준;
+
+public class 백준4673번 {
+    public static void main(String[] args) {
+
+        boolean[] check = new boolean[10001];
+
+        for (int i = 1; i < 10001; i++){
+            int n = d(i);
+
+            if(n < 10001){
+                check[n] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < 10001; i++) {
+            if (!check[i]) {
+                sb.append(i).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static int d(int n) {
+        int sum = n;
+        while(n!=0) {
+            sum = sum + (n%10);
+            n = n/10;
+        }
+        return sum;
+    }
+}
+
