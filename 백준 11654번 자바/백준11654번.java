dfc384b41c93ecf501e61a474d965d7312af9018
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-29 20:48:25 +0900

    아스키코드 출력

---
+++ 백준 11654번 자바/백준11654번.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class 백준11654번 {
+    public static void main(String[] args) throws IOException {
+        int a = System.in.read();
+        System.out.println(a);
+    }
+}
