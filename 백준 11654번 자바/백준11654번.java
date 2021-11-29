7e8f9e2614fd29990f7608200e35b34f45135fcb
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-29 20:55:48 +0900

    Update 백준11654번.java

--- 백준 11654번 자바/백준11654번.java
+++ 백준 11654번 자바/백준11654번.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class 백준11654번 {
     public static void main(String[] args) throws IOException {
         int a = System.in.read();//System.in은 byte값으로 문자 한개를 읽으면서 그에 해당하는 아스키코드 값을 저장함.
-        //주의할점은 꼭 예외처리를 해야함.
+        //주의할점은 꼭 예외처리를 해야함. 버퍼이기 때문.
         System.out.println(a);
     }
 }
