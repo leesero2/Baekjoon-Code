6be3483da72ec3b9d00ce2984d8f73d1abda095d
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-02 22:42:52 +0900

    Create 백준2675번.java

---
+++ 백준 2675번 자바/백준2675번.java
@@ -0,0 +1,26 @@
+package 백준문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class 백준2675번 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++) {
+            String[] str = br.readLine().split(" ");
+            int R = Integer.parseInt(str[0]);
+            String S = str[1];
+            for(int j = 0; j < S.length(); j++) {
+                for(int k = 0; k < R; k++) {
+                    System.out.print(S.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
