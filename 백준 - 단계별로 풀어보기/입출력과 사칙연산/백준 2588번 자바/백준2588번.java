dfd459864b9de3589e361176c8838d620dd8fb3f
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-11 21:13:53 +0900

    Create 백준2588번.java

---
+++ 백준 - 단계별로 풀어보기/입출력과 사칙연산/백준 2588번 자바/백준2588번.java
@@ -0,0 +1,15 @@
+package 입출력과사칙연산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준2588번 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(500%100);
+
+    }
+}
