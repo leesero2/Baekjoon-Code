201c55c709c5319a2782270eb36882cc689c07c2
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-11 21:26:10 +0900

    Create 백준10718번.java

---
+++ 백준 - 단계별로 풀어보기/입출력과 사칙연산/백준 10718번 자바/백준10718번.java
@@ -0,0 +1,9 @@
+package 입출력과사칙연산;
+
+public class 백준10718번 {
+    public static void main(String[] args) {
+        for(int i = 0; i<2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
