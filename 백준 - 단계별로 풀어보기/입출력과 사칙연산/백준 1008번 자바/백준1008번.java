424458e89aecd94464339ac8f6bc61dad87731fe
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-11 21:13:17 +0900

    Create 백준1008번.java

---
+++ 백준 - 단계별로 풀어보기/입출력과 사칙연산/백준 1008번 자바/백준1008번.java
@@ -0,0 +1,17 @@
+package 입출력과사칙연산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 백준1008번 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println((double) a/b);
+
+    }
+}
