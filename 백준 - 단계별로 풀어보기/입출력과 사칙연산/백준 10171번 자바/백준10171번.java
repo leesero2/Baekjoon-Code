14967f91496e832aaa0ad3395f533e1ea34f15ac
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-11 21:14:08 +0900

    Create 백준10171번.java

---
+++ 백준 - 단계별로 풀어보기/입출력과 사칙연산/백준 10171번 자바/백준10171번.java
@@ -0,0 +1,10 @@
+package 입출력과사칙연산;
+
+public class 백준10171번 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
