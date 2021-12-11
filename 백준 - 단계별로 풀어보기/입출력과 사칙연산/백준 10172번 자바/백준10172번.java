2c9cf2647672f75bf7699c2c84107dc284d486f0
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-11 21:14:26 +0900

    Create 백준10172번.java

---
+++ 백준 - 단계별로 풀어보기/입출력과 사칙연산/백준 10172번 자바/백준10172번.java
@@ -0,0 +1,11 @@
+package 입출력과사칙연산;
+
+public class 백준10172번 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
