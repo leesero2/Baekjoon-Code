558856e614da18c717f41280a6442defbd3a2cdb
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-16 23:41:38 +0900

    Create 백준2753번.java

---
+++ 백준 - 단계별로 풀어보기/If문/백준2753번.java
@@ -0,0 +1,17 @@
+package If문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준2753번 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if(a % 4 == 0 && a % 100 != 0 || a % 400 == 0){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
+}
