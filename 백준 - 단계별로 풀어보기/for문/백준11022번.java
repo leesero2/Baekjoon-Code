d53d7dd906879a9a6304bfc164d1cd899ab84bbc
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-12-21 23:36:44 +0900

    Create 백준11022번.java

---
+++ 백준 - 단계별로 풀어보기/for문/백준11022번.java
@@ -0,0 +1,20 @@
+package for문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 백준11022번 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        for(int i = 1; i<=a; ++i){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int sum = b+c;
+            System.out.println("Case #"+i+": "+b+" + "+c+" = "+sum);
+        }
+    }
+}
