9764451f822594e6104f047196f0efd80dfd87d7
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-01-04 23:17:55 +0900

    Create 백준1110번.java

---
+++ 백준 - 단계별로 풀어보기/while문/백준1110번.java
@@ -0,0 +1,24 @@
+package while문;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class 백준1110번 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        int copy = N;
+
+        do {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            cnt++;
+        } while (copy != N);
+
+        System.out.println(cnt);
+    }
+}
