c1fec909552e7bb99edd3fc65e4713a4bbe5bf5b
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-06-08 19:49:24 +0900

    Update 백준11720번.py

--- 파이썬/백준11720번.py
+++ 파이썬/백준11720번.py
@@ -1,2 +1,12 @@
-input()
-print(sum(map(int, input())))
+# input()
+# print(sum(map(int, input())))
+
+
+import sys
+a = int(sys.stdin.readline())
+b = sys.stdin.readline()
+
+n = 0
+for i in range(a):
+    n = int(b[i]) + n
+print(n)
