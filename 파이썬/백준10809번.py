4f575b3486966d55758fd26849f8ab9235144834
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-06-08 21:02:38 +0900

    Update 백준10809번.py

--- 파이썬/백준10809번.py
+++ 파이썬/백준10809번.py
@@ -1,6 +1,11 @@
+# 방법 1
 import sys
 s = sys.stdin.readline()
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 for i in alphabet:
     print(s.find(i), end = ' ')
 
+# 방법 2
+ss = input()
+for i in range(26):
+	print(ss.find(chr(97+i)), end=' ')
